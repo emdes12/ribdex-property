07af9b27738f259e06eca9d110a75e14fb9e6db6
Alawiye <naijabayz@gmail.com>
2024-11-05 02:34:04 +0100

    edit as necessary and also deleted unwanted files

---
+++ src/js/apland.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+// Initialize Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBB0cRuQsyRCCYfqhJyL0_KWAx2R-oKIqk",
+  authDomain: "ribdex-global-properties.firebaseapp.com",
+  databaseURL: "https://ribdex-global-properties-default-rtdb.firebaseio.com",
+  projectId: "ribdex-global-properties",
+  storageBucket: "ribdex-global-properties.appspot.com",
+  messagingSenderId: "443099008803",
+  appId: "1:443099008803:web:124afe57f81508a9d58696",
+  measurementId: "G-YLL2T1H5YN"
+};
